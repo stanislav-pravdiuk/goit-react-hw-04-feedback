9131d771ef52995c2972c2e97c03fb32c6f50050
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-05-09 14:27:32 +0300

    performs feedback refactoring

--- src/components/feedback/Feedback.jsx
+++ src/components/feedback/Feedback.jsx
@@ -1,70 +1,57 @@
-import React, { Component } from "react";
+import { useState } from "react";
 import FeedbackOptions from "components/fedback-options/FeedbackOptions";
 import Statistics from "../statistics/Statistics";
 import Section from "../section/Section";
 import Notification from "components/notification/Notification";
-import PropTypes from 'prop-types';
 import btns from './buttons.json'
 
-class Feedback extends Component{
-    static defaultProps = {
-        initialGood: 0,
-        initialNeutral: 0,
-        initialBad: 0,
-        initialTotal: 0,
-        initialPositiveFeedback: 0,
+function Feedback() {
+
+    let [good, setGood] = useState(0);
+    let [neutral, setNeutral] = useState(0);
+    let [bad, setBad] = useState(0);
+    let total = good + neutral + bad;
+    let positiveFeedback = Math.round(good * 100 / total)
+
+    function handleLeaveFeedback(event) {
+
+        switch (event.target.name) {
+            case 'good':
+                setGood(good + 1);
+                break;
+            
+            case 'neutral':
+                setNeutral(neutral + 1);
+                break;
+            
+            case 'bad':
+                setBad(bad + 1);
+                break;
+            
+            default:
+                return;
+        };
     };
 
-    state = {
-        good: this.props.initialGood,
-        neutral: this.props.initialNeutral,
-        bad: this.props.initialBad,
-        total: this.props.initialTotal,
-        positiveFeedback: this.props.initialPositiveFeedback,
-    };
-    
-    handleLeaveFeedback = (event) => {
-        const {name} = event.currentTarget
-
-        this.setState((prevState) => ({
-            [name]: prevState[name] + 1,
-        }));
-        this.countTotalFeedback()
-        this.countPositiveFeedbackPercentage()
-    };
-
-    countTotalFeedback = () => {
-        this.setState((prevState) => ({
-            total: prevState.good + prevState.bad + prevState.neutral
-        }))
-    }
-
-    countPositiveFeedbackPercentage = () => {
-        this.setState((prevState) => ({
-            positiveFeedback: Math.round(prevState.good * 100 / prevState.total)
-        }))
-    }
-
-    render() {
-        return (
+    return (
             <div className="feedback">
                 
                 <Section title=''>
                     <FeedbackOptions
-                        onLeaveFeedback={this.handleLeaveFeedback}
+                        onLeaveFeedback={handleLeaveFeedback}
                         options={btns.buttons}
                     />
                 </Section>
                 
                 <Section title="Statistics">
-                    {this.state.total !== 0
+                    {total !== 0
                         ? (
                             <Statistics
-                                good={this.state.good}
-                                neutral={this.state.neutral}
-                                bad={this.state.bad}
-                                total={this.state.total}
-                                positivePercentage={this.state.positiveFeedback}
+                                good={good}
+                                neutral={neutral}
+                                bad={bad}
+                                total={total}
+                                positivePercentage={positiveFeedback}
                             />
                         )
                     
@@ -73,15 +60,6 @@ class Feedback extends Component{
 
             </div>
         )
-    }
-};
-
-Feedback.propTypes = {
-    initialGood: PropTypes.number.isRequired,
-    initialNeutral: PropTypes.number.isRequired,
-    initialBad: PropTypes.number.isRequired,
-    initialTotal: PropTypes.number.isRequired,
-    initialPositiveFeedback: PropTypes.number.isRequired,
 };
 
 export default Feedback;
